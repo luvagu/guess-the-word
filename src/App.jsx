0e2350f9bd289c4e57a76fd77db363a12d4451f4
luvagu <luiavag@gmail.com>
2021-06-27 18:53:59 -0500

    adde state for bodyPartsToShow

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react'
 import Body from './components/body-parts/Body'
 
 function App() {
+  const [bodyPartsToShow, setBodyPartsToShow] = useState(0)
+
 	return (
 		<div className='wrapper'>
 			<header>
 				<h1>Guess The Word</h1>
 			</header>
 			<aside>
-        <Body />
+        <Body numOfBodyPartsToShow={bodyPartsToShow} />
       </aside>
 			<article>Main Content</article>
 		</div>
